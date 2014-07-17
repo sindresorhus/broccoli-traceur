3cf4a57bf620f2012c3aacf74173d88bc6d4344a
Gordon L. Hempton <ghempton@gmail.com>
2014-07-17 16:18:45 -0700

    pass filename into traceur compiler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var Filter = require('broccoli-filter');
 var traceur = require('traceur');
+var _ = require('lodash');
 
 function TraceurFilter(inputTree, options) {
 	if (!(this instanceof TraceurFilter)) {
@@ -17,8 +18,9 @@ TraceurFilter.prototype.constructor = TraceurFilter;
 TraceurFilter.prototype.extensions = ['js'];
 TraceurFilter.prototype.targetExtension = 'js';
 
-TraceurFilter.prototype.processString = function (str) {
-	var result = traceur.compile(str, this.options);
+TraceurFilter.prototype.processString = function (str, relativePath) {
+	var options = _.merge({filename: relativePath}, this.options);
+	var result = traceur.compile(str, options);
 
 	if (result.errors && result.errors.length > 0) {
 		throw result.errors[0];
